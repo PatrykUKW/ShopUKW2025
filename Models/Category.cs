60740fc998ca581ed35a9578864d2fb5e89e31af
Patryk <patryk.lazarski@ukw.edu.pl>
2025-03-03 16:31:37 +0100

    baza danych

---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopUKW2025.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Desc { get; set; }
+
+        public ICollection<Film> Films { get; set; }
+    }
+}
