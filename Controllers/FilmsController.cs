2b5aece37795f673c7972dd1e411f4c453975308
PatrUkw <patlaz@student.ukw.edu.pl>
2025-04-14 16:25:14 +0200

    Dodawanie filmów

--- Controllers/FilmsController.cs
+++ Controllers/FilmsController.cs
@@ -1,17 +1,54 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ShopUKW2025.DAL;
+using ShopUKW2025.Models;
 
 namespace ShopUKW2025.Controllers
 {
     public class FilmsController : Controller
     {
         FilmsContext db;
+        IWebHostEnvironment webHost;
 
-        public FilmsController(FilmsContext db)
+        public FilmsController(FilmsContext db, IWebHostEnvironment webHost)
         {
             this.db = db;
+            this.webHost = webHost;
         }
+
+        [HttpGet]
+        public IActionResult AddFilm()
+        {
+            var model = new AddFilmViewModel();
+            model.AllCategories = db.Categories.ToList();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult AddFilm(AddFilmViewModel model)
+        {
+            
+
+            var picFolderPath = Path.Combine(webHost.WebRootPath, "grafiki");
+
+            var uniqueName = Guid.NewGuid() + "_" + model.Poster.FileName;
+
+            var filePath = Path.Combine(picFolderPath, uniqueName);
+
+            model.Poster.CopyTo(new FileStream(filePath, FileMode.Create));
+
+
+            model.NewFilm.Poster = uniqueName;
+
+
+            db.Films.Add(model.NewFilm);
+            db.SaveChanges();
+
+            return RedirectToAction("Details", new { filmId = model.NewFilm.FilmId });
+        }
+
+
 
         public IActionResult Wszystkie()
         {
